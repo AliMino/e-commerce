6365a8dc4849c92ee0a835eaa00a7acb4478da3f
Ali <mino.ict@gmail.com>
2019-04-11 21:07:09 +0200

    Validate product's id passed in url

--- app/components/product-details/product-details.component.ts
+++ app/components/product-details/product-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, RouterLink } from '@angular/router';
 import { Product } from 'src/app/models/product';
 import { ProductsService } from 'src/app/services/productsService/products.service';
 import { CartService } from 'src/app/services/cartService/cart.service';
@@ -11,26 +11,32 @@ import { WishlistService } from 'src/app/services/wishListService/wishlist.servi
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
-  public id: string;
-  public products: Product[];
-  public product: Product;
+  public id: string;                      // passed in url
+  public products: Product[];             // all products from product service
+  public product: Product;                // this product or undefined
+  private onErrorNavigateTo: string = ""; // home page
+
   constructor(
     private productsService: ProductsService,
     private cartService: CartService,
     private wishlistService: WishlistService,
     private router: Router,
-    private route: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.products = this.productsService.getProducts();
-    this.id = this.route.params['value'].id;
-    this.extractProduct();
+    private route: ActivatedRoute) {
+    }
+    
+    ngOnInit() {
+      this.products = this.productsService.getProducts();
+      this.id = this.route.params['value'].id;
+      this.extractProduct();
+      if(!this.isValidProduct())
+        this.router.navigateByUrl(this.onErrorNavigateTo);
   }
-  extractProduct() {
+  // get product object from products service by its id
+  extractProduct(): void {
     this.products.forEach((product: Product) => {
       if(product.id == this.id)
         this.product = product;
-    })
+    });
   }
 
   addToWishlist(): void {
@@ -41,5 +47,9 @@ export class ProductDetailsComponent implements OnInit {
 
   addToCart(): void {
     this.cartService.addToCart(this.product);
+  }
+  // validating id passed in url
+  isValidProduct(): boolean {
+    return this.product != undefined;
   }
 }
