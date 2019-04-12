9e30b19fca8433c536025617e0de338c548a1b2a
Ali <mino.ict@gmail.com>
2019-04-12 23:16:36 +0200

    Add login and registeration routes

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { ProductsComponent } from './components/products/products.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path:'', component: ProductsComponent },
   { path: 'product/:id', component: ProductDetailsComponent },
   { path: 'checkout', component: CheckoutComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
