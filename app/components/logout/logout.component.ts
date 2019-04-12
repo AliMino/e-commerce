4f5007e0eba500b9e30edeed5c943bed21f7ce92
Ali <mino.ict@gmail.com>
2019-04-12 23:14:34 +0200

    Add logout component

---
+++ app/components/logout/logout.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/loginService/login.service';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.scss']
+})
+export class LogoutComponent implements OnInit {
+
+  constructor(private router: Router, private loginService: LoginService) { }
+
+  ngOnInit() {
+  }
+
+  logout() {
+    let answer = confirm('Are you sure you want to Logout?');
+    if (answer) {
+      localStorage.removeItem('currentUser');
+      this.loginService.setUsername('Guest');
+      this.router.navigate(['/login']);
+    } else {
+      this.router.navigate(['home']);
+    }
+  }
+
+}
