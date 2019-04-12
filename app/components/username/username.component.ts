db745234a9d35149ea9430906aa7670852740796
Ali <mino.ict@gmail.com>
2019-04-12 23:15:09 +0200

    Add username component

---
+++ app/components/username/username.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/loginService/login.service';
+import { BehaviorSubject } from 'rxjs';
+
+@Component({
+  selector: 'app-username',
+  templateUrl: './username.component.html',
+  styleUrls: ['./username.component.scss']
+})
+export class UsernameComponent implements OnInit {
+  
+  public userName: string;
+
+  constructor(private user: LoginService) {
+
+    
+    this.user.getUsername().subscribe(response => {
+      let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+      console.log("1", currentUser);
+      let user = '';
+      if (currentUser) {
+        user = currentUser.userName;
+        console.log("user->",user);
+        console.log("CurrentUser->",currentUser);
+      }
+      if (response == "Guest") {
+        this.userName = "Guest";
+        console.log("if", this.userName);
+      } else {
+        this.userName = response;
+        console.log("else",this.userName);
+      }
+    });
+  }
+  ngOnInit() {
+  }
+
+}
